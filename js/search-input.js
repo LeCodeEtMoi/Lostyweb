8cc43171dfc65b079f3c2114cb783c5b3f257ef8
LapetiteLigneDeCode <LapetiteLigneDeCode@tutanota.com>
2023-10-08 09:49:58 +0200

    Mise a jours de la bare de recherche

--- js/search-input.js
+++ js/search-input.js
@@ -1,34 +1,15 @@
-// Sélectionnez l'élément de saisie de recherche et le bouton de recherche
-const searchInput = document.getElementById('search-input');
-const searchButton = document.getElementById('search-button');
-
-// Sélectionnez toutes les images
-const images = document.querySelectorAll('.artwork');
-
-// Écoutez l'événement de clic sur le bouton de recherche
-searchButton.addEventListener('click', function () {
-  const searchTerm = searchInput.value.toLowerCase(); // Convertissez la recherche en minuscules
-
-  // Réinitialisez l'affichage de toutes les images
-  images.forEach(function (image) {
-    image.style.display = 'block';
-  });
-
-  // Vérifiez si la recherche est vide, puis affichez toutes les images si c'est le cas
-  if (searchTerm === '') {
-    images.forEach(function (image) {
-      image.style.display = 'block';
-    });
-    return; // Sortez de la fonction pour éviter la recherche supplémentaire
-  }
-
-  // Parcourez toutes les images pour les afficher ou les cacher en fonction de la recherche
-  images.forEach(function (image) {
-    const hasKeyword = image.querySelector('.keyword' + searchTerm); // Recherchez une classe de mot-clé correspondante
-    if (hasKeyword) {
-      image.style.display = 'block'; // Affichez l'image si la classe de mot-clé correspond
-    } else {
-      image.style.display = 'none'; // Cachez l'image sinon
+// JavaScript code
+function search_animal() {
+    let input = document.getElementById('searchbar').value
+    input=input.toLowerCase();
+    let x = document.getElementsByClassName('image');
+      
+    for (i = 0; i < x.length; i++) { 
+        if (!x[i].innerHTML.toLowerCase().includes(input)) {
+            x[i].style.display="none";
+        }
+        else {
+            x[i].style.display="list-item";                 
+        }
     }
-  });
-});
+}
